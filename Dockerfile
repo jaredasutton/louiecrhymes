59260c846179b963dd5e694721173b36cb861c46
Jared A Sutton <jaredasutton@Jareds-MBP.home>
2020-02-12 00:07:18 -0500

    Implement waiting on Mongo container initialization before starting server container.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ WORKDIR /usr/src/app
 
 COPY package.json /usr/src/app/
 RUN npm install
+RUN npm install -g nc
 
 COPY . /usr/src/app
-
-CMD [ "node", "server" ]
--- db/mongoClient.js
+++ db/mongoClient.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const schemae = require('./schemae');
-const { MONGO_PRIMARY_UN, MONGO_PRIMARY_PW, MONGO_PORT } = process.env;
-const connectString = `mongodb://${MONGO_PRIMARY_UN}:${MONGO_PRIMARY_PW}@localhost:${MONGO_PORT}/louiec1`;
+const {
+  MONGO_PRIMARY_UN,
+  MONGO_PRIMARY_PW,
+  MONGO_HOST,
+  MONGO_PORT
+} = process.env;
+console.log(`Preparing to connect to localhost:${MONGO_PORT}`);
+const connectString = `mongodb://${MONGO_PRIMARY_UN}:${MONGO_PRIMARY_PW}@${MONGO_HOST}:${MONGO_PORT}/louiec1`;
 mongoose.connect(connectString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
